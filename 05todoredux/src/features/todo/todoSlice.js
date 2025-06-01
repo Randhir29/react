8c120fac9124e3ab96c6ed25032a51ce1efa8f05
Randhir29 <randhirveerrajput@gmail.com>
2025-06-01 21:18:39 +0000

    redux

---
+++ 05todoredux/src/features/todo/todoSlice.js
@@ -0,0 +1,31 @@
+import {createSlice, nanoid } from '@reduxjs/toolkit';
+
+const initialState = {
+    todos: [{id: 1, text: "Hello world",isEditable:false}]
+}
+
+
+
+export const todoSlice = createSlice({
+    name: 'todo',
+    initialState,
+    reducers: {
+        addTodo: (state, action) => {
+            const todo = {
+                id: nanoid(), 
+                text: action.payload
+            }
+            state.todos.push(todo)
+        },
+        removeTodo: (state, action) => {
+            state.todos = state.todos.filter((todo) => todo.id !== action.payload )
+        },
+        editTodo:(state,action)=>{
+            state.todos = state.todos.map((todo)=> todo.id===action.payload ? {...todo, isEditable:!todo.isEditable} : todo)
+        }
+    }
+})
+
+export const {addTodo, removeTodo, editTodo} = todoSlice.actions
+
+export default todoSlice.reducer
